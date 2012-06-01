e6dd1b486b452d0c1f5470d4f986d28eaf14ce9b
Jim Rootham <jrootham@gmail.com>
2012-06-01 16:52:00 -0400

    Initial code commit

---
+++ spec/mhSpec.js
@@ -0,0 +1,36 @@
+//  Test hm.js
+
+describe("Jasmine is present", function() {
+  it("true is true", function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe("Simple functions to build HTML strings", function(){
+  it("single without options", function(){
+    expect(mh.single("br", [])).toEqual("<br />");
+  });
+  
+  it("nv == make name value pair", function() {
+    expect(mh.nv("type", "text")).toEqual({name:"type", value:"text"});
+  });
+
+  it("single with 1 option", function() {
+    expect(mh.single("input", [mh.nv("type", "text")]))
+      .toEqual('<input type="text" />');
+  });
+
+  it("single with 2 options", function() {
+    expect(mh.single("input", [mh.nv("type", "text"), mh.nv("name","foo")]))
+      .toEqual('<input type="text" name="foo" />');
+  });
+
+  it("paired without options", function(){
+    expect(mh.paired("div", "contents", [])).toEqual('<div>contents</div>');
+  });
+
+  it("paired with 2 options", function(){
+    expect(mh.paired("div", "contents", [mh.nv("class","foo"),mh.nv("id","thing")]))
+      .toEqual('<div class="foo" id="thing">contents</div>');
+  });
+});
---
+++ src/mh.js
@@ -0,0 +1,47 @@
+// HTML Maker
+
+//  Simple functions to ease construction of HTML strings
+
+var mh = {
+// single : a tag with no contents, includes its own terminator and options
+
+// tag      :  HTML tag
+// options  : an array of objects with name and value variables
+
+  single : function(tag, options) {
+    var result = '<' + tag;
+    
+    for (i = 0 ; i < options.length ; i++) {
+      result += ' ' + options[i].name + '="' + options[i].value + '"';
+    }
+    result += ' />';
+    return result;
+  },
+
+// paired : a tag with contents and options
+
+// tag      :  HTML tag
+// contents :  contents of tag
+// options  : an array of objects with name and value variables
+  
+  paired : function(tag, contents, options) {
+    var result = '<' + tag;
+    for (i = 0 ; i < options.length ; i++) {
+      result += ' ' + options[i].name + '="' + options[i].value + '"';
+    }
+    result += '>';
+    result += contents;
+    result += '</' + tag + '>';
+    return result;
+  },
+  
+  
+// a convenience function to create an options object
+
+// name   : name of option
+// value  : value of option
+
+  nv : function(name, value) {
+    return {name: name, value: value};
+  }
+};
